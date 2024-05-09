0fef03ac43fc3bb8f595c9af951e7d3e1a9ba419
lbenoit31 <157414735+lbenoit31@users.noreply.github.com>
2024-05-09 19:12:01 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -77,8 +77,8 @@ var init = function (window) {
             if ( circle.x > canvas.width ) {
                 circle.x = 0;
             }
-            if (circle.y < 0) {
-                circle.y = canvas.width
+            if (circle.x < 0) {
+                circle.x = canvas.width
             }
             if (circles.y < 0) {
                 circle.y = canvas.height
